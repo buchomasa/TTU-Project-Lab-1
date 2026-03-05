7fd0399afb5a86aa7dc814dfcca2bf24a1c7c86e
buchomasa <zrgottschalk@gmail.com>
2026-03-05 12:40:12 -0600

    Add team selection functionality in Team.py

---
+++ Role/Team.py
@@ -0,0 +1,17 @@
+from machine import Pin
+
+teamColor = Pin(2, Pin.IN, Pin.PULL_UP)
+def pickTeam():
+    purpleTeam = False
+    yellowTeam = False
+    if teamColor.value() == 0:
+        purpleTeam = True
+        yellowTeam = False
+        print("Purple team selected")
+    
+    else:
+        purpleTeam = False
+        yellowTeam = True
+        print("Yellow team selected")
+    
+pickTeam()
